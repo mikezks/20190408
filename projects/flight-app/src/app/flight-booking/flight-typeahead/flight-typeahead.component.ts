ac675e80a8ea79fbc32e96984b86844f5198feca
mikezks <michael.zikes@intauria.com>
2019-04-08 13:57:57 +0200

    RxJS

--- projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { timer, Observable, Subscription, Subject } from 'rxjs';
-import { take, tap, share, takeUntil } from 'rxjs/operators';
+import { timer, Observable, Subscription, Subject, pipe, empty } from 'rxjs';
+import { take, tap, share, takeUntil, debounceTime, delay, filter, map, switchMap } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-api';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-typeahead',
@@ -11,11 +14,16 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   timer$: Observable<number>;
   timerSubscription: Subscription;
   destroy$ = new Subject<boolean>();
+
+  control = new FormControl();
+  flights$: Observable<Flight[]>;
+  loading: boolean;
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
   
   ngOnInit() {
-    this.rxjsDemo();
+    // this.rxjsDemo();
+    this.initTypeahead();
   }
   
   rxjsDemo(): void {
@@ -37,10 +45,55 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
       );
       
     }
+
+    initTypeahead(): void {
+      const customFilter = (noChars: number) =>
+        pipe(
+          map((value: string) => {
+            if (value.length > noChars) {
+              return value;
+            }
+            return empty();
+          })
+        );
+
+      this.control.valueChanges
+        .pipe(
+          debounceTime(300),
+          // delay(1000),
+          filter((value: string) => value.length > 2),
+          // customFilter(2),
+          tap(() => this.loading = true),
+          switchMap(
+            value => this.load(value)
+/*               .pipe(
+                map(flights => ({
+                  flights, value
+                }))
+              ) */
+          ),
+          tap(() => this.loading = false)
+        )
+        .subscribe(
+          console.log
+        )
+    }
+
+    load(from: string):Observable<Flight[]>  {
+      const url = "http://www.angular.at/api/flight";
+  
+      const params = new HttpParams()
+                          .set('from', from);
+  
+      const headers = new HttpHeaders()
+                          .set('Accept', 'application/json');
+  
+      return this.http.get<Flight[]>(url, {params, headers});
+    };
     
     ngOnDestroy(): void {
       // this.timerSubscription.unsubscribe();
-      this.destroy$.next(true);
+      // this.destroy$.next(true);
     }
   }
   
