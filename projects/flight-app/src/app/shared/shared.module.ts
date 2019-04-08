4e1bfb6b7b6ab1ddf80eaa2211fa4f0b2e17d3ba
mikezks <michael.zikes@intauria.com>
2019-04-08 11:29:53 +0200

    Init

---
+++ projects/flight-app/src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { CustomCheckboxModule } from './custom-checkbox/custom-checkbox.module';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {ModuleWithProviders} from '@angular/core/src/metadata/ng_module';
+import {CityPipe} from './pipes/city.pipe';
+import { TabsModule } from './tabs/tabs.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CustomCheckboxModule,
+    TabsModule
+  ],
+  declarations: [
+    CityPipe,
+  ],
+  exports: [
+    CityPipe,
+    CustomCheckboxModule,
+    TabsModule
+  ]
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    }
+  }
+
+  static forChild(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    }
+  }
+
+}
